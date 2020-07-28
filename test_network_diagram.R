bbbc4e21cf96fe6736d363715b7ec3103ca4a8f9
Will Sutton <sutton_999@hotmail.com>
2020-07-28 21:40:53 +0100

    appending workflow for datafam tweets by user json

--- test_network_diagram.R
+++ test_network_diagram.R
@@ -2,6 +2,7 @@
 
 library(jsonlite)
 library(rtweet)
+library(dplyr)
 
 ## search for #rstats tweets
 rstats <- search_tweets("#rstats", n = 200)
@@ -14,3 +15,18 @@ rstats_net2 <- aggregate(to ~ from, rstats_net, list)
 
 rstats_net_json <- toJSON(rstats_net2, pretty = TRUE)
 write(rstats_net_json, "data/#rstats_network.json")
+
+
+datafam <- readRDS("data/#datafam_tweets.RDS")
+
+datafam$handle <- paste0("@",datafam$screen_name)
+
+datafam_nodes <- datafam %>%
+  select(handle) %>% 
+  count(handle)
+
+names(datafam_nodes)[names(datafam_nodes) == "n"] <- "tweets"
+datafam_nodes_json <- toJSON(datafam_nodes, pretty = TRUE)
+write(datafam_nodes_json, "data/#datafam_nodes.json")
+
+#datafam_nodes$tweets <- datafam_nodes$n
