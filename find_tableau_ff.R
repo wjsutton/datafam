581811b7f077a4eae96160678d2a868599eaa383
wjsutton <sutton_999@hotmail.com>
2020-09-10 17:07:06 +0100

    testing tableau public api

---
+++ find_tableau_ff.R
@@ -0,0 +1,88 @@
+# Get data from Twitter
+
+library(rtweet)
+library(dplyr)
+library(jsonlite)
+
+source("functions/function_get_and_save_tweets.R")
+source("functions/function_get_tableau_profile_url.R")
+source("functions/function_get_tableau_public_api_extract.R")
+
+# Get data
+get_and_save_tweets(text = "#tableau", count = 10000, path = "data")
+get_and_save_tweets(text = "#datafam", count = 10000, path = "data")
+get_and_save_tweets(text = "#makeovermonday", count = 10000, path = "data")
+
+# Read and merge data
+tableau <- readRDS("data/#tableau_tweets.RDS")
+datafam <- readRDS("data/#datafam_tweets.RDS")
+makeovermonday <- readRDS("data/#makeovermonday_tweets.RDS")
+
+all_tweets <- rbind(tableau,datafam,makeovermonday)
+
+all_links <- unlist(all_tweets$urls_expanded_url)
+
+# remove NAs
+all_links <- all_links[!is.na(all_links)]
+
+# find tableau links
+all_links <- all_links[grepl('public.tableau',all_links)]
+
+profiles <- get_tableau_profile_url(all_links)
+
+profile_urls <- unique(profiles$profile)
+
+profile_name <- ifelse(substr(profile_urls,nchar(profile_urls)-2,nchar(profile_urls))=='#!/',
+  substr(profile_urls,36,nchar(profile_urls)-3),
+  substr(profile_urls,36,nchar(profile_urls)))
+
+profile_name <- unique(profile_name[!is.na(profile_name)])
+
+for(i in seq(profile_name)){
+  profile_df <- get_tableau_profile_api_extract(profile_name[i])
+  print(i)
+  if(i == 1){
+    df <- profile_df
+  }
+  if(i >= 1){
+    df <- rbind(df,profile_df)
+  }
+}
+
+write.csv(df,'tableau_ff.csv',row.names = F)
+
+
+
+profile_call <- paste0('https://public.tableau.com/profile/api/',profile_name[i])
+
+profile_data <- jsonlite::fromJSON(profile_call)
+
+profile_following <- profile_data$totalNumberOfFollowing
+profile_followers <- profile_data$totalNumberOfFollowers
+
+profile_twitter <- profile_data$websites$url[grepl('twitter',profile_data$websites$url)]
+profile_linkedin <- profile_data$websites$url[grepl('linkedin',profile_data$websites$url)]
+
+profile_last_publish <- profile_data$lastPublishDate
+profile_visible_workbooks <- profile_data$visibleWorkbookCount
+
+profile_following <- ifelse(length(profile_following)==1,profile_following,0)
+profile_followers <- ifelse(length(profile_followers)==1,profile_followers,0)
+
+profile_twitter <- ifelse(length(profile_twitter)==1,profile_twitter,'')
+profile_linkedin <- ifelse(length(profile_linkedin)==1,profile_linkedin,'')
+
+profile_last_publish <- ifelse(length(profile_last_publish)==1,profile_last_publish,0)
+profile_visible_workbooks <- ifelse(length(profile_visible_workbooks)==1,profile_visible_workbooks,0)
+
+profile_df <- data.frame(name=profile_name[i],
+                         profile_url=paste0('https://public.tableau.com/profile/',profile_name[i],'#!/'),
+                         api_call=profile_call,
+                         followers=profile_followers,
+                         following=profile_following,
+                         twitter=profile_twitter,
+                         linkedin=profile_linkedin,
+                         last_publish=profile_last_publish,
+                         visible_workbooks=profile_visible_workbooks,
+                         stringsAsFactors = F)
+
--- functions/function_get_tableau_public_api_extract.R
+++ functions/function_get_tableau_public_api_extract.R
@@ -1,30 +1,9 @@
-# Tableau Public API workings
-
-library(jsonlite)
-
-profile <- 'https://public.tableau.com/profile/api/will7508'
-
-# limits to 300, can use index +1 for next 300
-workbooks <- 'https://public.tableau.com/profile/api/will7508/workbooks?count=300&index=0'
-
-# Profile Data extract 
-profile_data <- fromJSON(profile)
-
-following <- profile_data$totalNumberOfFollowing
-followers <- profile_data$totalNumberOfFollowers
-
-twitter <- profile_data$websites$url[grepl('twitter',profile_data$websites$url)]
-linkedin <- profile_data$websites$url[grepl('linkedin',profile_data$websites$url)]
-
-last_publish <- profile_data$lastPublishDate
-visible_workbooks <- profile_data$visibleWorkbookCount
-
 # Note workbooks includes invisible_workbooks, determined by the 'showInProfile' TRUE/FALSE flag
 
 library(jsonlite)
 
 get_tableau_profile_api_extract <- function(profile_name){
-
+  
   
   profile_call <- paste0('https://public.tableau.com/profile/api/',profile_name)
   
@@ -39,6 +18,15 @@ get_tableau_profile_api_extract <- function(profile_name){
   profile_last_publish <- profile_data$lastPublishDate
   profile_visible_workbooks <- profile_data$visibleWorkbookCount
   
+  profile_following <- ifelse(length(profile_following)==1,profile_following,0)
+  profile_followers <- ifelse(length(profile_followers)==1,profile_followers,0)
+  
+  profile_twitter <- ifelse(length(profile_twitter)==1,profile_twitter,'')
+  profile_linkedin <- ifelse(length(profile_linkedin)==1,profile_linkedin,'')
+  
+  profile_last_publish <- ifelse(length(profile_last_publish)==1,profile_last_publish,0)
+  profile_visible_workbooks <- ifelse(length(profile_visible_workbooks)==1,profile_visible_workbooks,0)
+  
   profile_df <- data.frame(name=profile_name,
                            profile_url=paste0('https://public.tableau.com/profile/',profile_name,'#!/'),
                            api_call=profile_call,
@@ -52,4 +40,3 @@ get_tableau_profile_api_extract <- function(profile_name){
   return(profile_df)
 }
 
-get_tableau_profile_api_extract('will7508')
