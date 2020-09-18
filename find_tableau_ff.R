8b90d64cb445e7c50fad372df117a2dc7f8d232e
wjsutton <sutton_999@hotmail.com>
2020-09-18 16:59:19 +0100

    updating find tableau ff file

--- find_tableau_ff.R
+++ find_tableau_ff.R
@@ -48,10 +48,65 @@ for(i in seq(profile_name)){
     df <- rbind(df,profile_df)
   }
 }
-
+df <- unique(df)
 write.csv(df,'tableau_ff.csv',row.names = F)
 
+source("functions/function_get_last_n_tableau_vizs_extract.R")
+source("functions/function_get_tableau_viz_screenshot_url.R")
 
+latest_viz <- filter(df,visible_workbooks>0)
+
+for(i in 1:nrow(latest_viz)){
+  print(i)
+  viz <- get_last_n_tableau_vizs_extract(profile_name = latest_viz$name[i],n =5)
+  
+  if(i == 1){
+    viz_df <- viz
+  }
+  if(i != 1){
+    viz_df <- rbind(viz_df,viz)
+  }
+}
+
+#get_tableau_viz_screenshot_url(viz_df$workbooks[1])
+
+for(i in 1:length(viz_df$workbooks)){
+  viz_b <- viz_df$workbooks[i]
+  
+  # Case 2 'profile vizhome'
+  if(!is.na(str_locate(pattern="vizhome",viz_b)[1])){
+    
+    # Start of "?"
+    viz_b <- gsub("\\?publish=yes","",viz_b)
+    
+    # Start of profile
+    profile_start_pos <- str_locate_all(pattern="profile",viz_b)[[1]][1,1]
+    
+    # End of "vizhome"
+    vizhome_end_pos <- str_locate_all(pattern="vizhome",viz_b)[[1]][1,2]
+    two_letters_of_dash <- substr(viz_b,vizhome_end_pos+2,vizhome_end_pos+3)
+    
+    knit <- paste0(substr(viz_b,1,profile_start_pos-1)
+                   ,'static/images/',two_letters_of_dash,'/'
+                   ,substr(viz_b,vizhome_end_pos+2,nchar(viz_b))
+                   ,'/1.png')
+  }
+  if(i == 1){
+    imgs <- knit
+  }
+  if(i != 1){
+    imgs <- c(imgs,knit)
+  }
+}
+
+viz_df$screenshot <- imgs
+df$profile_name <- df$name
+merged <- left_join(viz_df,df[,c("profile_name","profile_url",'followers')], by = ("profile_name" = "profile_name"))
+
+merged$img_and_profile_link <- paste0("<a href='",merged$profile_url,"'>",
+                                        "<img src='",merged$screenshot,"'></a>")
+
+write.csv(merged,'tableau_ff_v2.csv',row.names = F)
 
 profile_call <- paste0('https://public.tableau.com/profile/api/',profile_name[i])
 
